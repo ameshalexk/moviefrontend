d480560d57c75d52711c2eb9f806c4979a4b6d7c
Denell Williams <DenellM51@gmail.com>
2022-02-16 16:55:35 -0500

    login component.ts updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   public username:string = '';
-  public password: string = '';
-
+  public password:string = '';
 
 
   constructor() { }
@@ -17,14 +17,16 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  if(form.valid) {
-    console.log( form.value);
-    console.log( "username=" + this.username);
-    console.log( "password=" + this.password);
+  onSubmit(form:NgForm) {
+
+    if(form.valid) {
+      console.log( form.value );
+      console.log("username = " + this.username);
+      console.log("password = " + this.password);
+      
     }
     else {
       console.log("Form not valid")
     }
+  }
 }
-
-
