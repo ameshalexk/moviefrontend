d13d063fc8cd03e7f8f6798f52802b9cf8bb2a90
ameshalexk <ameshalexusa@gmail.com>
2022-02-17 10:22:26 -0600

    active link logic added for navbar toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button'
-import {MatButtonToggleModule} from '@angular/material/button-toggle'
-import {MatIconModule} from '@angular/material/icon'
-import {MatToolbarModule} from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatListModule } from '@angular/material/list';
+
 
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatListModule
 ]
 
 @NgModule({
