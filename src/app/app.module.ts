4ac6eb16ca16e707bccbf59011e19946f84c5bfa
Sidney Henderson <shende39@uncc.edu>
2022-02-17 10:54:39 -0500

    Fixed login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { environment } from 'src/environments/environment.prod';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { CreateMovieFormComponent } from './create-movie-form/create-movie-form.component';
-=======
 import { HttpClientModule } from '@angular/common/http'; //need to import http
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
->>>>>>> efd50a9b6cf3ae8ea825a1f212a90db00205f40b
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LoginComponent,
-    CreateMovieFormComponent
-=======
-    MovieListComponent
->>>>>>> efd50a9b6cf3ae8ea825a1f212a90db00205f40b
+    MovieListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    //ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule // used for doing API calls
   ],
   providers: [],
-  bootstrap: [AppComponent, LoginComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-constructor(private angularAuth) {
-
-}
-
-
